821cc4ace286f26152281041a0d8674b6e3bf544
Corpus-PreciousHannah <corpus.precioushannah1@gmail.com>
2025-04-08 20:38:16 +0800

    Add a file

---
+++ main.py
@@ -0,0 +1,27 @@
+#TODO(Name): Display the Main Menu Options
+def display_menu():
+    pass
+
+#TODO(Corpus): Display all movie records
+def list_all_movies():
+    pass
+
+#TODO(Corpus): Add a new movie to the list
+def add_movie():
+    pass
+
+#TODO(Pineda): Update movie details
+def update_movie():
+    pass
+
+#TODO(Morales): Delete a movie from the list
+def delete_movie():
+    pass
+
+#TODO(Name 2): Search for a movie by title
+def search_movie():
+    pass
+
+#TODO(Name 1): Handle Program Loop and User Interface
+def main():
+    pass
